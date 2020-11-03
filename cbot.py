99babc92cb220e770a1d445e0e1dd5e4853a1052
Frost <FFrost@users.noreply.github.com>
2020-11-03 04:31:18 -0500

    fixes for updated version of d.py

--- cbot.py
+++ cbot.py
@@ -228,9 +228,11 @@ class CBot(commands.Bot):
 
             # update the new config with the old values if they exist
             for k, v in disk_dicts.items():
+                # if the disk config has an outdated key, discard it
                 if (k not in default_dicts.keys()):
                     continue
                 
+                # otherwise, try to save the old value
                 if ("." in k):
                     try:
                         utils.nested_set(data, k.split("."), v)
@@ -317,7 +319,8 @@ class CBot(commands.Bot):
             if (s.unavailable): # can't retrieve info about guild
                 print("\t{id} - guild is unavailable!".format(id=s.id))
             else:
-                print("\t{name} owned by {owner}#{ownerid}".format(name=s.name, owner=s.owner.name, ownerid=s.owner.discriminator))
+                owned_by = f" owned by {s.owner.name}#{s.owner.discriminator}" if s.owner else ""
+                print(f"\t{s.name}{owned_by}")
     
     async def on_error(self, event, *args, **kwargs):
         trace = traceback.format_exc()
@@ -387,74 +390,6 @@ class CBot(commands.Bot):
 
             await self.messaging.error_alert(e)
 
-        except Exception as e:
-            await self.messaging.error_alert(e)
-    
-    async def on_member_join(self, member):
-        try:
-            if (member == self.user):
-                return
-            
-            await self.messaging.msg_admin_channel("{time} {name} [{uid}] joined".format(time=utils.get_cur_time(),
-                                                                                         name=utils.format_member_name(member),
-                                                                                         uid=member.id),
-                                                                                         member.guild)
-            
-        except Exception as e:
-            await self.messaging.error_alert(e)
-    
-    async def on_member_remove(self, member):
-        try:
-            if (member == self.user):
-                return
-
-            msg = f"{utils.get_cur_time()} {utils.format_member_name(member)} [{member.id}] left"
-
-            if (randint(0, 5) == 0):
-                msg = f"{utils.get_cur_time()} \N{CRAB} {utils.format_member_name(member)} [{member.id}] is gone \N{CRAB}"
-            
-            await self.messaging.msg_admin_channel(msg, member.guild)
-        
-        except Exception as e:
-            await self.messaging.error_alert(e)
-    
-    async def on_guild_join(self, guild):
-        try:
-            await self.messaging.message_developer("{time} CBot joined guild {name}#{id}".format(time=utils.get_cur_time(),
-                                                                                                             name=guild.name,
-                                                                                                             id=guild.id))
-
-            perms = dict(guild.me.guild_permissions)
-            all_perms = self.REQUIRED_PERMISSIONS + self.VOICE_PERMISSIONS + self.OPTIONAL_PERMISSIONS
-            perms_we_dont_have = []
-
-            for perm in all_perms:
-                if (not perms[perm]):
-                    perms_we_dont_have.append(perm)
-
-            msg = f"Hi, thanks for adding me to your guild `{guild.name}`. The minimum permissions I need to function are " \
-                  f"{utils.format_code_brackets(self.REQUIRED_PERMISSIONS).replace('_', ' ')}.\n" \
-                  f"For voice support, I need {utils.format_code_brackets(self.VOICE_PERMISSIONS).replace('_', ' ')} in the voice channel you want me to join.\n" \
-                  f"For additional commands, I need {utils.format_code_brackets(self.OPTIONAL_PERMISSIONS).replace('_', ' ')}\n"
-
-            msg += (f"I currently don't have {utils.format_code_brackets(perms_we_dont_have).replace('_', ' ')} permissions."
-                    if (len(perms_we_dont_have) > 0) else
-                    "I have all the permissions I need. Thanks!")
-
-            try:
-                await guild.owner.send(msg)
-            except discord.errors.Forbidden:
-                pass
-        
-        except Exception as e:
-            await self.messaging.error_alert(e)
-    
-    async def on_guild_remove(self, guild):
-        try:
-            await self.messaging.message_developer("{time} CBot was removed from guild {name}#{id}".format(time=utils.get_cur_time(),
-                                                                                                                       name=guild.name,
-                                                                                                                       id=guild.id))
-        
         except Exception as e:
             await self.messaging.error_alert(e)
                 
